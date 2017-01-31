b52b261ac8f84465ec8e53c11b84170817c2b4b0
Arthur Schiwon <blizzz@arthur-schiwon.de>
2017-02-01 00:08:47 +0100

    Add extended attribute. requires schema change
    Signed-off-by: Arthur Schiwon <blizzz@arthur-schiwon.de>

--- inst.sh
+++ inst.sh
@@ -37,12 +37,12 @@ data+="&configData[ldapAgentPassword]="`urlEncode "$NC_LDAP_PWD"`
 data+="&configData[ldapBase]="`urlEncode "$ldap_base"`
 data+="&configData[ldapBaseUsers]="`urlEncode "cn=users,$ldap_base"`
 data+="&configData[ldapBaseGroups]="`urlEncode "cn=groups,$ldap_base"`
-data+="&configData[ldapUserFilter]="`urlEncode "(&(objectclass=nextcloudUser)(nextcloudEnabled=TRUE))"`
+data+="&configData[ldapUserFilter]="`urlEncode "(&(objectclass=nextcloudUser)(nextcloudEnabled=1))"`
 data+="&configData[ldapUserFilterMode]=1"
-data+="&configData[ldapLoginFilter]="`urlEncode "(&(objectclass=nextcloudUser)(nextcloudEnabled=TRUE)(uid=%uid))"`
+data+="&configData[ldapLoginFilter]="`urlEncode "(&(objectclass=nextcloudUser)(nextcloudEnabled=1)(uid=%uid))"`
 data+="&configData[ldapLoginFilterMode]=1"
 data+="&configData[ldapLoginFilterUsername]=1"
-data+="&configData[ldapGroupFilter]="`urlEncode "(&(objectclass=nextcloudGroup)(nextcloudEnabled=TRUE))"`
+data+="&configData[ldapGroupFilter]="`urlEncode "(&(objectclass=nextcloudGroup)(nextcloudEnabled=1))"`
 data+="&configData[ldapGroupFilterMode]=1"
 data+="&configData[ldapGroupFilterObjectclass]=nextcloudGroup"
 data+="&configData[ldapUserDisplayName]=uid"
@@ -71,7 +71,49 @@ curl -v --cacert /etc/univention/ssl/ucsCA/CAcert.pem -X GET -H "OCS-APIREQUEST:
 
 # moar settings: quota,homeAttr,groupSearchAttr
 
-# worst case: fire occ using joinscript_run_in_container
+joinscript_register_schema
+
+univention-directory-manager container/cn create "$@" --ignore_exists \
+    --position "cn=custom attributes,cn=univention,$ldap_base" \
+    --set name=nextcloud
+
+univention-directory-manager settings/extended_attribute create "$@" \
+    --position "cn=nextcloud,cn=custom attributes,cn=univention,$ldap_base" --set module="users/user" \
+    --set ldapMapping='nextcloudEnabled' \
+    --set objectClass='nextcloudUser' \
+    --set name='nextcloudUserEnabled' \
+    --set shortDescription='Nextcloud enabled' \
+    --set longDescription='whether user or group should be available in Nextcloud ' \
+    --set translationShortDescription='"de_DE" "Nextloud aktiviert"' \
+    --set translationLongDescription='"de_DE" "Der Benutzer kann auf Nextcloud zugreifen"' \
+    --set tabName='Nextcloud' \
+    --set translationTabName='"de_DE" "Nextcloud"' \
+    --set overwriteTab='0' \
+    --set valueRequired='0' \
+    --set CLIName='nextcloudEnabled' \
+    --set syntax='boolean' \
+    --set default="1" \
+    --set tabAdvanced='1' \
+    --set mayChange='1' \
+    --set multivalue='0' \
+    --set deleteObjectClass='0' \
+    --set tabPosition='1' \
+    --set overwritePosition='0' \
+    --set doNotSearch='0' \
+    --set hook='None' || \
+univention-directory-manager settings/extended_attribute modify "$@" \
+	--dn "cn=nextcloudUserEnabled,cn=nextcloud,cn=custom attributes,cn=univention,$ldap_base" \
+	--set tabAdvanced='1' \
+	--set default="1"
+
+JoinUsersFilter="(&(|(&(objectClass=posixAccount) (objectClass=shadowAccount)) (objectClass=univentionMail) (objectClass=sambaSamAccount) (objectClass=simpleSecurityObject) (&(objectClass=person) (objectClass=organizationalPerson) (objectClass=inetOrgPerson))) (!(uidNumber=0)) (!(|(uid=*$) (uid=nextcloud-systemuser) (uid=join-backup) (uid=join-slave))) (!(objectClass=nextcloudUser)))"
+
+for dn in $(udm users/user list "$@" --filter "$JoinUsersFilter" | sed -ne 's/^DN: //p') ; do
+	echo "modyfing $dn .."
+	udm users/user modify "$@" --dn "$dn" \
+		--set nextcloudEnabled="1" \
+		--set nextcloudQuota=""
+done
 
 joinscript_save_current_version
 exit 0
