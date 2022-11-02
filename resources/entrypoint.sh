68a33f9fe1b6d66b03f53540e442399fe13a488f
szaimen <szaimen@e.mail.de>
2022-11-02 11:23:07 +0100

    move to entrypoint script
    Signed-off-by: szaimen <szaimen@e.mail.de>

--- resources/entrypoint.sh
+++ resources/entrypoint.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/env bash
+if [ -f "/var/www/html/occ" ]; then
+    # Apply one-click-instance settings...
+    sudo -u www-data php /var/www/html/occ config:system:set one-click-instance --value=true --type=bool
+    sudo -u www-data php /var/www/html/occ config:system:set one-click-instance.user-limit --value=100 --type=int
+    sudo -u www-data php /var/www/html/occ config:system:set one-click-instance.link --value="https://nextcloud.com/univention/"
+    sudo -u www-data php /var/www/html/occ app:enable support
+fi
 service cron start && \
 rm /var/run/apache2/apache2.pid 2>/dev/null || true && \
 /usr/sbin/apache2ctl -D FOREGROUND
